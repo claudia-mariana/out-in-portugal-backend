9c79342a1e19059ae91386ff38295a11855f5806
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-11-29 09:34:09 +0000

    implemented automatic login when sign up

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -21,58 +21,49 @@ const saltRounds = 10;
 router.post("/signup", (req, res, next) => {
   const { email, password, name } = req.body;
 
-  // Check if email or password or name are provided as empty strings
   if (email === "" || password === "" || name === "") {
     res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
 
-  // This regular expression check that the email is of a valid format
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
   if (!emailRegex.test(email)) {
     res.status(400).json({ message: "Provide a valid email address." });
     return;
   }
 
-  // This regular expression checks password for special characters and minimum length
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
     res.status(400).json({
-      message:
-        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      message: "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
     });
     return;
   }
 
-  // Check the users collection if a user with the same email already exists
   User.findOne({ email })
     .then((foundUser) => {
-      // If the user with the same email already exists, send an error response
       if (foundUser) {
         res.status(400).json({ message: "User already exists." });
         return;
       }
 
-      // If email is unique, proceed to hash the password
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPassword = bcrypt.hashSync(password, salt);
 
-      // Create the new user in the database
-      // We return a pending promise, which allows us to chain another `then`
       return User.create({ email, password: hashedPassword, name });
     })
     .then((createdUser) => {
-      // Deconstruct the newly created user object to omit the password
-      // We should never expose passwords publicly
       const { email, name, _id } = createdUser;
 
-      // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const payload = { _id, email, name };
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "6h",
+      });
 
-      // Send a json response containing the user object
-      res.status(201).json({ user: user });
+      res.status(201).json({ user: { email, name, _id }, authToken: authToken });
     })
-    .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+    .catch((err) => next(err));
 });
 
 // POST  /auth/login - Verifies email and password and returns a JWT
